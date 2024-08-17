522cd45cf9f52a139305899c6b715b5e45645e3d
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-17 17:13:39 +0300

    make build

---
+++ index.3ca04913.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".population"),t=0;e.forEach(function(e){var n=e.innerText.replace(/,/g,"");t+=+n});var n=t/e.length,o=document.querySelector(".total-population"),r=document.querySelector(".average-population");o.innerText=t,r.innerText=n;
+//# sourceMappingURL=index.3ca04913.js.map
